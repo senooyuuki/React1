1240cdfc0b7ce1611438fe4593f21d3f4f64791d
senooyuuki <82793456+senooyuuki@users.noreply.github.com>
2022-07-20 13:07:29 +0900

    完成

--- src/compornents/ColorfulMessage.jsx
+++ src/compornents/ColorfulMessage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 const ColorfullMessage = (props) => {
-  console.log(props);
+  const { color, children } = props;
   const contentStyle = {
-    color: "blue",
+    color: color,
     fontSize: "18px"
   };
-  return <p style={contentStyle}>お元気ですか</p>;
+  return <p style={contentStyle}>{children}</p>;
 };
 
 export default ColorfullMessage;
